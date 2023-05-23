ae1a292469e7b9684e8f7d6ce26304c025790b93
Im young <111354589+im-young@users.noreply.github.com>
2023-05-23 11:32:59 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -10,8 +10,15 @@
 # ON A.BOARD_ID = B.BOARD_ID
 
 SELECT A.TITLE, A.BOARD_ID, B.REPLY_ID, B.WRITER_ID, B.CONTENTS, DATE_FORMAT(B.CREATED_DATE, '%Y-%m-%d')
+-- 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일을 조회
 FROM USED_GOODS_BOARD A
 JOIN USED_GOODS_REPLY B
+# ON A.BOARD_ID = B.BOARD_ID
+
+# FROM USED_GOODS_REPLY B
+# JOIN USED_GOODS_BOARD A
 ON A.BOARD_ID = B.BOARD_ID
 WHERE DATE_FORMAT(A.CREATED_DATE, '%Y-%m') = '2022-10'
+-- 2022년 10월에 작성된 
 ORDER BY 6, 1
+-- 결과는 댓글 작성일을 기준으로 오름차순 정렬해주시고, 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬해주세요.
