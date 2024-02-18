13a0bb46e6101a4235d982456809cf8415f7974b
Im young <111354589+im-young@users.noreply.github.com>
2024-02-19 00:20:18 +0900

    [level 1] Title: 여러 기준으로 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+# SELECT ANIMAL_ID,NAME,DATETIME
+# from ANIMAL_INS 
+# group by NAME
+# order by DATETIME desc
+
+SELECT ANIMAL_ID,NAME,DATETIME
+from ANIMAL_INS 
+order by name, datetime desc; 
