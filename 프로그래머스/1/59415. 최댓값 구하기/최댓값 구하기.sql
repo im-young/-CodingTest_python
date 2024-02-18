e10436204e688a5e215b3f9d88a6fbd73f8db7b4
Im young <111354589+im-young@users.noreply.github.com>
2024-02-19 01:25:30 +0900

    [level 1] Title: 최댓값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
+++ 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
@@ -1,4 +1,8 @@
 -- 코드를 입력하세요
-SELECT DATETIME
-from ANIMAL_INS 
-order by DATETIME desc limit 1; 
+# SELECT DATETIME
+# from ANIMAL_INS 
+# order by DATETIME desc limit 1;
+
+-- 최근일수록 DATETIME TYPE은 큰 값이다.
+SELECT MAX(DATETIME) 시간
+FROM ANIMAL_INS;
