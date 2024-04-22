f206b52a220d332b5d4d1752f5995df57a50e771
Im young <111354589+im-young@users.noreply.github.com>
2024-04-22 19:45:41 +0900

    [level 1] Title: 여러 기준으로 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
+++ 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
@@ -1,9 +1,4 @@
 -- 코드를 입력하세요
-# SELECT ANIMAL_ID,NAME,DATETIME
-# from ANIMAL_INS 
-# group by NAME
-# order by DATETIME desc
-
-SELECT ANIMAL_ID,NAME,DATETIME
-from ANIMAL_INS 
-order by name, datetime desc; 
+select ANIMAL_ID,	NAME,	DATETIME
+from ANIMAL_INS
+order by NAME, datetime desc;
