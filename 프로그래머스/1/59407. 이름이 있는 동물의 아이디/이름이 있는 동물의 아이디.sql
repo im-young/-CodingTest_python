0c13acaff315a88a590b2efd98611c77a782bd38
Im young <111354589+im-young@users.noreply.github.com>
2024-04-23 18:26:54 +0900

    [level 1] Title: 이름이 있는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
+++ 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID
-from ANIMAL_INS 
-where NAME is not null 
-order by ANIMAL_ID asc ; 
+select ANIMAL_ID
+from ANIMAL_INS
+where name is not null
+order by ANIMAL_ID asc
